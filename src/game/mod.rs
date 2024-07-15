227d5dac04c57cb77546ca59d64229faa813ed8b
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-07-15 11:18:36 -0700

    Reorganizes game module tests.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,21 +1,5 @@
-pub use game::*;
-
-mod game;
-
-mod quiz;
+pub mod game;
+pub mod quiz;
 #[cfg(test)]
-mod tests {
-	use crate::game::Game;
+mod tests;
 
-	#[test]
-	fn init() {
-		let game = Game::new();
-		assert_eq!(game.as_quizzes().len(), 1235);
-	}
-	#[test]
-	fn quiz() {
-		let game = Game::new();
-		let quiz = game.as_quiz(4);
-		assert_eq!(quiz.glyph(), "示");
-	}
-}
---
+++ src/game/tests/mod.rs
@@ -0,0 +1,37 @@
+mod game {
+	use crate::game::game::Game;
+
+	#[test]
+	fn init() {
+		let game = Game::new();
+		assert_eq!(game.as_quizzes().len(), 1235);
+	}
+	#[test]
+	fn quiz() {
+		let game = Game::new();
+		let quiz = game.as_quiz(4);
+		assert_eq!(quiz.glyph(), "示");
+	}
+}
+
+mod quiz {
+	use chrono::Utc;
+
+	use crate::game::quiz::{Quiz, QuizEvent};
+	use crate::ka::parse_kanji;
+
+	#[test]
+	fn check() {
+		let now = Utc::now();
+		let kanji = parse_kanji();
+		let quiz = Quiz::new(&kanji[4]);
+		assert_eq!(quiz.unsolved_answers_len(now), 2);
+		assert_eq!(quiz.fails_len(now), 0);
+		let quiz = quiz.after_event(QuizEvent::Solve("いち".into(), now));
+		assert_eq!(quiz.unsolved_answers_len(now), 2);
+		assert_eq!(quiz.fails_len(now), 1);
+		let quiz = quiz.after_event(QuizEvent::Solve("シ".into(), now));
+		assert_eq!(quiz.unsolved_answers_len(now), 1);
+		assert_eq!(quiz.fails_len(now), 1);
+	}
+}
